1f626286d7b5b27f05086232fcad3dbde4603116
varun <varunsiravuri@gmail.com>
2025-09-07 17:50:10 +0530

    created the db and some

---
+++ apps/engine/types.ts
@@ -0,0 +1,15 @@
+export type Cmd =
+  | {
+      id: string;
+      kind: "create-position";
+      asset: string;
+      type: "long" | "short";
+      margin: number;
+      leverage: number;
+      slippage?: number;
+    }
+  | { id: string; kind: "close-position"; orderId: string }
+  | { id: string; kind: "get-balance-usd" }
+  | { id: string; kind: "get-balances" };
+
+export type Reply = any;
